16673fb126884771806ed5ef816fdb81113a1406
Matthew Horner <13524665@student.uts.edu.au>
2021-05-17 02:39:41 +1000

    some pop files

---
+++ IoTBay/web/db/PopulateProducts.sql
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: 17/05/2021
+ */
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: 17/05/2021
+ */
+
+INSERT INTO ADMIN1.PRODUCTS (ID, "NAME", PRICE, DESCRIPTION, QUANTITY, "TYPE") 
+	VALUES (32587562, 'Dell 2 in one', 5000, 'Its a computer', 3, 'Computer'),
+	 (123123, 'Alienware thing', 3232, 'Its a bad computer', 1, 'Computer'),
+	 (4235424, 'Thinkpad', 3214, 'Its an ok computer', 3, 'Computer'),
+	 (3402424, 'Dumbpad', 3232, 'Goes beep', 4, 'Computer'),
+	 (23123, 'Dell something', 1000, 'Beep boop', 3, 'Computer'),
+	 (1203231, 'Iphone', 3232, 'Crap', 3, 'Phone'),
+	 (2133123, 'Samsung Galaxy s 8', 2344, 'Meh', 3, 'Phone'),
+	 (3313213, 'Gaming phone', 4343, 'SHOT', 4, 'Phone'),
+	 (42339424, 'Mouse', 4243, 'sdfsfsfs', 3, 'Stuff'),
+         (12833123, 'Alienware thing', 3232, 'Its a bad computer', 1, 'Computer'),
+	 (4233424, 'Thinkpad', 3214, 'Its an ok computer', 3, 'Computer'),
+	 (343721424, 'Dumbpad', 3232, 'Goes beep', 4, 'Computer'),
+	 (23611323, 'Dell something', 1000, 'Beep boop', 3, 'Computer'),
+	 (1232315, 'Iphone', 3232, 'Crap', 3, 'Phone'),
+	 (2132123, 'Samsung Galaxy s 8', 2344, 'Meh', 3, 'Phone'),
+	 (33133213, 'Gaming phone', 4343, 'SHOT', 4, 'Phone'),
+	 (42333424, 'Mouse', 4243, 'sdfsfsfs', 3, 'Stuff'),
+	 (42343323, 'Keyboard', 12, 'dsdgsg', 4, 'Stuff')
---
+++ IoTBay/web/db/PopulateUsers.sql
@@ -0,0 +1,60 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: 17/05/2021
+ */
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  User
+ * Created: 17/05/2021
+ */
+INSERT INTO ADMIN1.USERS ("NAME", EMAIL, PASSWORD, PHONE, ACCOUNTTYPE) 
+	VALUES ('Matthew', 'matthew@horner.com', 'password1', '13547757', 'c'),
+
+	 ('John', 'johnman@man.com', 'password', '3215565', 'c'),
+
+	 ('Martin', 'Martinman@man.com', 'password12', '1244215', 'c'),
+
+	 ('Josh', 'josh@email.com', 'pass', '424142', 'c'),
+
+	 ('Martha', 'Marth@girl.com', 'pass12', '124124', 'c'),
+
+	 ('Dillan', 'danmean@gil.com', 'password4', '124524', 'c'),
+
+	 ('Sam', '412421@cs.com', 'password', '42412', 'c'),
+
+	 ('black', 'black@yellow.com', 'password12', '1424124', 'c'),
+
+	 ('white', 'white@yellow.com', 'password21', '214124', 'c'),
+
+	 ('blue', 'blue@yellow.com', 'password43', '1241241', 'c'),
+
+	 ('grey', 'grey@yellow.com', 'password32', '2412412', 'c'),
+
+	 ('greeen', 'green@yellow.com', 'password', '421421', 'c'),
+
+	 ('black', 'blaeck@yellow.com', 'password12', '1424124', 'c'),
+
+	 ('whitee', 'whiewte@yellow.com', 'password21', '214124', 'c'),
+
+	 ('bluee', 'bluse@yellow.com', 'password43', '1241241', 'c'),
+
+	 ('greey', 'gresy@yellow.com', 'password32', '2412412', 'c'),
+
+	 ('greeen', 'grbeen@yellow.com', 'password', '421421', 'c'),
+
+	 ('blufee', 'blusde@yellow.com', 'password43', '1241241', 'c'),
+
+	 ('greeay', 'gressy@yellow.com', 'password32', '2412412', 'c'),
+
+	 ('greaeen', 'grbaeen@yellow.com', 'password', '421421', 'c')
+
